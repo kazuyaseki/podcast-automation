13febfa482306f50cdbdb6ac8142a746d7c26d37
kazuyaseki <track613armory@gmail.com>
2024-10-15 00:24:06 +0900

    fixed to use japanese model

--- src/generate_summary_voice.py
+++ src/generate_summary_voice.py
@@ -5,9 +5,13 @@ from elevenlabs import ElevenLabs, VoiceSettings
 ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 
 def generate_summary_voice(audio_file_path: str, output_path: str) -> str:
-    gemini_client = genai.GenerativeModel("gemini-1.5-pro-latest")
+    gemini_client = genai.GenerativeModel("gemini-1.5-pro-002")
     audio_file = genai.upload_file(path=audio_file_path)
-    prompt = "ポッドキャストの内容を要約したナレーション文を書いてください"
+    prompt = """ポッドキャストの内容を要約したナレーション文を書いてください。
+このナレーション文はポッドキャストの冒頭で説明するのに使われます。
+
+## 動画内で登場する固有名詞
+seya, kagaya"""
     print("generating narration text...")
     response = gemini_client.generate_content([prompt, audio_file])
 
@@ -16,9 +20,11 @@ def generate_summary_voice(audio_file_path: str, output_path: str) -> str:
         api_key=ELEVENLABS_API_KEY,
     )
     narration_audio = elevenlabs_client.text_to_speech.convert(
-        voice_id="pMsXgVXv3BLzUgSXRplE",
+        model_id="eleven_turbo_v2_5",
+        voice_id="8kS8nwk1TQdxvQOmfTZA",
         optimize_streaming_latency="0",
         output_format="mp3_22050_32",
+        language_code="ja",
         text=response.text,
         voice_settings=VoiceSettings(
             stability=0.1,
